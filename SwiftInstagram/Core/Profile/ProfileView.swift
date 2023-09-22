094c299ac0b2c0d687d5d3c9bdc9249e1b2d959f
JimSweeneyJr <jimsweeney566@gmail.com>
2023-09-21 20:54:01 -0400

    Added navigation and setting hamburger

--- SwiftInstagram/Core/Profile/ProfileView.swift
+++ SwiftInstagram/Core/Profile/ProfileView.swift
@@ -15,100 +15,88 @@ struct ProfileView: View {
         .init(.flexible(), spacing: 1)
     ]
     var body: some View {
-        VStack {
-            // header
-            VStack(spacing: 10) {
-                // pic and stats
-                HStack {
-                    Image("RachProfilePic")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 80, height: 80)
-                        .clipShape(Circle())
+        NavigationView {
+            ScrollView {
+                VStack {
+                    // header
+                    VStack(spacing: 10) {
+                        // pic and stats
+                        HStack {
+                            Image("RachProfilePic")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 80, height: 80)
+                                .clipShape(Circle())
+                            
+                            Spacer()
+                            
+                            HStack(spacing: 8) {
+                                
+                                StatView(value: 3, title: "Posts")
+                                
+                                StatView(value: 3, title: "Followers")
+                                
+                                StatView(value: 3, title: "Following")
+                                
+                            }
+                        }
+                        .padding(.horizontal)
+                        // name and bio
+                        VStack(alignment: .leading, spacing: 4){
+                            Text("Rachael Clark")
+                                .font(.footnote)
+                                .fontWeight(.semibold)
+                            
+                            Text("Work is Life!")
+                                .font(.footnote)
+                                
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.horizontal)
+                        // action button
+                        
+                        Button {
+                            
+                        } label: {
+                            Text("Edit Profile")
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .frame(width: 360, height: 32)
+                                .foregroundColor(.black)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 6)
+                                        .stroke(Color.gray, lineWidth: 1))
+
+                        }
+                        
+                        Divider()
+                    }
+                    // post grid view
                     
-                    Spacer()
-                    
-                    HStack(spacing: 8) {
+                    LazyVGrid(columns: gridItems, spacing: 1) {
                         
-                        StatView(value: 3, title: "Posts")
-                        
-                        StatView(value: 3, title: "Followers")
-                        
-                        StatView(value: 3, title: "Following")
-                        
+                        ForEach(0 ... 35, id: \.self) { index in
+                            Image("bridge")
+                                .resizable()
+                                .scaledToFill()
+                        }
                     }
                 }
-                .padding(.horizontal)
-                // name and bio
-                VStack(alignment: .leading, spacing: 4){
-                    Text("Rachael Clark")
-                        .font(.footnote)
-                        .fontWeight(.semibold)
-                    
-                    Text("Work is Life!")
-                        .font(.footnote)
-                        
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.horizontal)
-                // action button
-                
-                Button {
-                    
-                } label: {
-                    Text("Edit Profile")
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .frame(width: 360, height: 32)
-                        .foregroundColor(.black)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 6)
-                                .stroke(Color.gray, lineWidth: 1))
-
+                .navigationTitle("Profile")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "line.3.horizontal")
+                                .foregroundColor(.black)
+                        }
+                    }
                 }
                 
-                Divider()
             }
-            // post grid view
-            
-            LazyVGrid(columns: gridItems, content: {
-                
-                Image("bridge")
-                    .resizable()
-                    .scaledToFill()
-                
-                Image("bridge")
-                    .resizable()
-                    .scaledToFill()
-                
-                Image("bridge")
-                    .resizable()
-                    .scaledToFill()
-                Image("bridge")
-                    .resizable()
-                    .scaledToFill()
-                
-                Image("bridge")
-                    .resizable()
-                    .scaledToFill()
-                
-                Image("bridge")
-                    .resizable()
-                    .scaledToFill()
-                Image("bridge")
-                    .resizable()
-                    .scaledToFill()
-                
-                Image("bridge")
-                    .resizable()
-                    .scaledToFill()
-                
-                Image("bridge")
-                    .resizable()
-                    .scaledToFill()
-            })
-                
-            } // LAZYVGRID
+        } // LAZYVGRID
         }
     
 }
