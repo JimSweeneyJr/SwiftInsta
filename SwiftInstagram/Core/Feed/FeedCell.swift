be691e500b3093bbe0d76691e599c0beb2d8be57
JimSweeneyJr <jimsweeney566@gmail.com>
2023-09-21 21:27:55 -0400

    added feed view and feed cell

---
+++ SwiftInstagram/Core/Feed/FeedCell.swift
@@ -0,0 +1,81 @@
+//
+//  FeedCell.swift
+//  SwiftInstagram
+//
+//  Created by James Sweeney on 9/21/23.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    var body: some View {
+        VStack {
+            // image + username
+            HStack {
+                Image("RachProfilePic")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                
+                Text("Rachael")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                
+                Spacer()
+                
+            }
+            .padding(.leading, 8)
+            
+            // post image
+            
+            Image("bridge")
+                .resizable()
+                .scaledToFill()
+                .frame(height: 400)
+                .clipShape(Rectangle())
+            
+            // action buttons
+            HStack(spacing: 16) {
+                
+                Button {
+                    print("Like Post")
+                } label: {
+                    Image(systemName: "heart")
+                        .imageScale(.large)
+                    
+                }
+                
+                Button {
+                    print("Comment on Post")
+                } label: {
+                    Image(systemName: "bubble.right")
+                        .imageScale(.large)
+                }
+                
+                Button {
+                    print("Share Post")
+                } label: {
+                    Image(systemName: "paperplane")
+                        .imageScale(.large)
+                }
+                
+                Spacer()
+                
+            }
+            .padding(.leading, 8)
+            .padding(.top, 4)
+            .foregroundColor(.black)
+            
+            // likes label
+            
+            // caption label
+        }
+    }
+}
+
+struct FeedCell_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedCell()
+    }
+}
