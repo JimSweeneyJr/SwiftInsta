fdb7218a8dac1aad52a0fff805e2acdbf1fffb30
JimSweeneyJr <jimsweeney566@gmail.com>
2023-09-24 21:23:06 -0400

    Add search and explore view

--- SwiftInstagram/Core/Feed/FeedView.swift
+++ SwiftInstagram/Core/Feed/FeedView.swift
@@ -9,11 +9,29 @@ import SwiftUI
 
 struct FeedView: View {
     var body: some View {
-        ScrollView {
-            LazyVStack {
-                ForEach(0 ... 10, id: \.self) { post in
-                    FeedCell()
-                    
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 32) {
+                    ForEach(0 ... 10, id: \.self) { post in
+                        FeedCell()
+                        
+                    }
+                }
+                .padding(.top, 8)
+            }
+            .navigationTitle("Feed")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Image("logo")
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                        .padding(.bottom, 4)
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Image(systemName: "paperplane")
+                        .imageScale(.large)
                 }
             }
         }
