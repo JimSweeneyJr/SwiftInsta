d22d3eb7fe6da271e24a2918ee4000f26018d477
JimSweeneyJr <jimsweeney566@gmail.com>
2023-09-25 18:04:55 -0400

    Add an email has been added

---
+++ SwiftInstagram/Core/Authentication/AddEmailView.swift
@@ -0,0 +1,20 @@
+//
+//  AddEmailView.swift
+//  SwiftInstagram
+//
+//  Created by James Sweeney on 9/25/23.
+//
+
+import SwiftUI
+
+struct AddEmailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddEmailView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddEmailView()
+    }
+}
